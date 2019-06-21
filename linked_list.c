254464b3d0a0032647c40a97e2f3691e39029ad8
Miguel Oliveira <32375361+MiguelDordio@users.noreply.github.com>
2019-06-21 23:18:23 +0100

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,11 +1,40 @@
 #include "linked_list.h"
 
-node *search(node *head, int key){
-    node *tmp_node = head;
 
-    while(tmp_node != NULL && tmp_node->data != key){
+linked_list *create_new(){
+    linked_list *linked;
+    if((linked = malloc(sizeof(linked_list))) == NULL)
+        return NULL;
+    linked->head = NULL;
+    linked->size = 0;
+    return linked;
+}
+
+
+bool empty(linked_list *linked){
+    return linked->head == NULL;
+}
+
+
+bool exists(linked_list *linked, char *airport_code){
+    node *tmp_node = linked->head;
+
+    while(tmp_node != NULL && strcmp(tmp_node->airport_code, airport_code) != 0){
         if(tmp_node->next == NULL) {
-            printf("key does not exist\n");
+            return false;
+        }else
+            tmp_node = tmp_node->next;
+    }
+
+    return true;
+}
+
+
+node *get_node(linked_list *linked, char *airport_code){
+    node *tmp_node = linked->head;
+
+    while(tmp_node != NULL && strcmp(tmp_node->airport_code, airport_code) != 0){
+        if(tmp_node->next == NULL) {
             return NULL;
         }else
             tmp_node = tmp_node->next;
@@ -14,34 +43,38 @@ node *search(node *head, int key){
     return tmp_node;
 }
 
-node *insert(node *head, int key){
+void insert_new(linked_list *linked, char *airport_code){
 
     node *new_node = malloc(sizeof(node));
-    new_node->data = key;
-    new_node->next = head;
+    strcpy(new_node->airport_code, airport_code);
+    new_node->flights_size = 0;
+    new_node->flight_time = 0;
+    new_node->flights = malloc(sizeof(flight) * 20);
+    new_node->next = linked->head;
 
-    head = new_node;
+    linked->size++;
+    linked->head = new_node;
 
-    return head;
 }
 
-bool delete(node **head, int key) {
-    if (*head == NULL) return false;
+bool delete(linked_list *linked, char *airport_code) {
+    if (linked->head == NULL) return false;
 
-    node *tmp = *head;
+    node *tmp = linked->head;
     node *prev = NULL;
 
-    while (tmp->data != key && tmp->next != NULL) {
+    while (strcmp(tmp->airport_code, airport_code) != 0 && tmp->next != NULL) {
         prev = tmp;
         tmp = tmp->next;
     }
 
-    if (tmp->data == key) {
+    if (strcmp(tmp->airport_code, airport_code) == 0) {
         if (prev)
             prev->next = tmp->next;
         else
-            *head = tmp->next;
+            linked->head = tmp->next;
         free(tmp);
+        linked->size--;
         return true;
     }
 
@@ -49,9 +82,27 @@ bool delete(node **head, int key) {
 }
 
 
-void printReverse(node* head) {
-    if (head == NULL)
+void pop(linked_list *linked){
+    if(linked->head != NULL){
+        if(linked->head->next != NULL){
+            linked->head = linked->head->next;
+        }else{
+            linked->head = NULL;
+        }
+        linked->size--;
+    }
+}
+
+
+void print(linked_list *linked) {
+    printf("\nlinked list size: %d\n", linked->size);
+    if (linked->head == NULL)
         return;
-    printReverse(head->next);
-    printf("data: %d\n", head->data);
+    node *current = linked->head;
+    while(current != NULL){
+        printf("airport: %s\n", current->airport_code);
+        if(current->next == NULL)
+            return;
+        current = current->next;
+    }
 }
